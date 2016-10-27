d2a7caf8bf1184d8dc5ce804c7015120c950ae67
taral <ssh 54904d514382ec968f000054@rmsg42-appns.rhcloud.com>
2016-10-27 23:12:52 +0530

    Assignement 2

--- asn2.py
+++ asn2.py
@@ -75,7 +75,7 @@ def set_up_cities(names=['City 0', 'City 1', 'City 2', 'City 3', 'City 4', 'City
     city_list = []
     for n in enumerate(names):
         """n[0] = 0,1,2,3,4 etc."""
-        """n[0] = Name of the city."""
+        """n[1] = Name of the city."""
         city_list += [ make_city(n[1],con[n[0]]) ]
 
     return city_list
